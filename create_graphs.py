ad68074a7cd27b6dce386df11133f20243dc98b7
Dylan Mayor <dylan@Dylans-MacBook-Pro.local>
2021-02-24 21:26:44 +0000

    added graphing functions

--- create_graphs.py
+++ create_graphs.py
@@ -1,4 +1,6 @@
 import json
+from matplotlib import pyplot
+import numpy
 
 OUTPUT_FILENAME = "output.txt"
 HEADER_INDICATOR = "--"
@@ -16,12 +18,68 @@ def to_dict(file) -> dict:
 
         if current_header:
             if line.strip() != "":
-                recording_result = line.split(";")[1].split()[0]
-                output_dict[current_header].append(recording_result)
+                recording_result = line.split(";")[1].strip("\n ns")
+                output_dict[current_header].append(int(recording_result))
 
     return output_dict
 
 
+def average(dct: dict):
+    tuple = ()
+
+    for value in dct.values():
+        tuple += (sum(value) / len(value), )
+
+    return tuple
+
+
+def create_bar_chart_read(data):
+    pyplot.title("Read performance")
+    pyplot.ylabel("time taken (ns)")
+
+    __finalise_chart(data)
+
+
+def create_bar_chart_write(data):
+    pyplot.title("write performance")
+    pyplot.ylabel("time taken (ns)")
+
+    __finalise_chart(data)
+
+
+def create_bar_chart_enc(data):
+    pyplot.title("Construct/Destruct performance")
+    pyplot.ylabel("time taken (ns)")
+
+    __finalise_chart(data)
+
+
+
+def __finalise_chart(data):
+    systems = 'SGX Driver', 'SGX KVM', 'Virtualized SGX'
+    index = numpy.arange(len(systems))
+    bar_width = 0.4
+
+    pyplot.bar(index, data, color='g', width=bar_width)
+    pyplot.xticks(index, systems)
+    pyplot.show()
+
+
+def bundle_data(sgx_driver, sgx_kvm, virt_sgx):
+    return [
+            (sgx_driver[0], sgx_kvm[0], virt_sgx[0]), (sgx_driver[1], sgx_kvm[1], virt_sgx[1]),
+            (sgx_driver[2], sgx_kvm[2], virt_sgx[2])
+    ]
+
+
+
 if __name__ == '__main__':
     output_file = open(OUTPUT_FILENAME)
-    print(json.dumps(to_dict(output_file), indent=2))
+    result_dict = to_dict(output_file)
+
+    print(json.dumps(result_dict, indent=2))
+    print(average(result_dict))
+
+    data = bundle_data(average(result_dict), average(result_dict), average(result_dict))
+
+    create_bar_chart_read(data[0])
