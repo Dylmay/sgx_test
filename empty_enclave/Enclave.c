80646ec81e1bd2c9f3f70ab28b692ec311b073e1
Dylan Mayor <dylmay456@icloud.com>
2021-03-04 17:18:54 +0000

    added variable enclave size for better const_dest testing

---
+++ empty_enclave/Enclave.c
@@ -0,0 +1,11 @@
+#include "Enclave.h"
+#include "../untrusted/sgx_perf.h"
+
+
+char data_array[DATA_SIZE];
+
+void ecall_empty()
+{
+
+}
+
---
+++ empty_enclave/Enclave.h
@@ -0,0 +1,14 @@
+#ifndef _ENCLAVE_H_
+#define _ENCLAVE_H_
+
+#include <assert.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* !_ENCLAVE_H_ */
